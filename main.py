8778c2a3230db4ede4d2c5e5aed90a0636e90979
Bumsoo Kim <kbeom00@gmail.com>
2024-05-17 17:54:05 +0900

    clean up main.py

--- main.py
+++ main.py
@@ -1,7 +1,3 @@
-import os
-
-char_dic= {}
-
 # return txt which is all lower case
 def lower_txt(txt):
     new_txt_list = []
@@ -9,7 +5,6 @@ def lower_txt(txt):
         new_txt_list.append(char.lower())
 
     return new_txt_list
-
 
 # make dictionary which refers key to character and value to occurence
 def make_dic(txt):
@@ -20,7 +15,6 @@ def make_dic(txt):
         else:
             txt_dic[char] = 1
     return txt_dic
-
 
 # count number of item in given list
 def count_items(list):
@@ -36,9 +30,11 @@ def convert_to_list_of_dic(dic):
         new_list.append({key: dic[key]})
     return new_list
 
+# function for sort()
 def sort_by_value(dic):
     return list(dic.values())[0]
 
+# return only alphabet list
 def only_alphabet(given_list):
     new_list = []
     for dict in given_list:
@@ -46,6 +42,7 @@ def only_alphabet(given_list):
             new_list.append(dict)
     return new_list
 
+# final print function
 def print_message(num, path, given_list):
     print(f"--- Begin report of {path} ---")
     print(f"{num} words found in the document\n")
@@ -54,23 +51,25 @@ def print_message(num, path, given_list):
         print(f"The '{list(dictionary.keys())[0]}' character was found {list(dictionary.values())[0]} times")
     print("--- End report ---")
 
+
+# MAIN
 def main():
     path = "./books/frankenstein.txt"
     # open text file
     with open(path) as file:
         file_contents = file.read()
 
-    # COUNT ITEM IN LIST
+    ## COUNT ITEM IN LIST ##
 
     # create single char list
     f_word_list = []
     f_word_list = file_contents.split()
+
     # count items
     num_of_items = count_items(f_word_list)
 
 
-
-    # COUNT CHAR IN TEXT FILE
+    ## COUNT CHAR IN TEXT FILE ##
 
     # make lowercase
     lower_txt(file_contents)
@@ -87,6 +86,9 @@ def main():
     # sort dic
     converted_list.sort(reverse=True, key=sort_by_value)
     print(converted_list)
+
+
+    ## PRINT TO CONSOLE ##
 
     # print out to console
     print_message(num_of_items, path, converted_list)
